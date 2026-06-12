8e22ddcb03d57b079a2324e47e96d0c283a5db8f
Michael Haggerty <mhagger@github.com>
2026-06-12 16:06:58 +0200

    stageJoiner: new helper type for creating pipes between stages
    The old code used a `stageStarter` type to help with starting up
    stages. There are two awkward things about that approach:
    
    * The pipe that is needed to join stages depends on the two adjacent
      stages, not on the stdin and stdout of a single stage. So
      `stageStarter` wasn't able to figure out what pipe was needed; that
      logic kindof needed to live in the `Pipeline` type.
    
    * Since the read and write ends of a pipe are created together, the
      `stageStarter` could also not be as helpful in closing those streams
      if there was an error.
    
    So instead, use a new helper type, `stageJoiner`, to figure out how to
    join two adjacent stages together. The `stageJoiner` can now do part
    of the work for us:
    
    * `stageJoiner.validate()` checks that the adjacent stages'
      requirements are met WRT whether they need stdin/stdout to be
      supplied at all.
    
    * `stageJoiner.createPipe()` figures out what kind of pipe to create
      for the two adjacent stages.
    
    * `stageJoiner.closePipe()` closes the pipe (if it has already been
      created and needs closing) on errors.
    
    This isn't a panacea; for example, some loops have to iterate over
    stages, and some over stage joiners. But I think that it's less
    confusing this way.

---
+++ pipe/stage_joiner.go
@@ -0,0 +1,126 @@
+package pipe
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
+
+// stageJoiner is a helper type that helps join two adjacent stages
+// together. stageJoiners[i] tells how to connect stage `i-1` to stage
+// `i`. From the point of view of stages, `stageJoiners[i].nextStdin`
+// and `stageJoiners[i+1].prevStdout` are the input and output
+// streams, respectively, of `stage[i]`. The first and last elements
+// of `stageJoiners` manage `p.stdin` and `p.stdout`, respectively.
+// Schematically, the data flows through like this:
+//
+//	p.stdin == stageJoiners[0].nextStdin →
+//	   stage[0] →
+//	   stageJoiners[1].prevStdout → stageJoiners[1].nextStdin →
+//	   stage[1] →
+//	   stageJoiners[2].prevStdout → stageJoiners[2].nextStdin →
+//	   stage[2] →
+//	   ... →
+//	   stageJoiners[i].prevStdout → stageJoiners[i].nextStdin →
+//	   stage[i] →
+//	   stageJoiners[i+1].prevStdout → stageJoiners[i+1].nextStdin →
+//	   ... →
+//	   stageJoiners[len(stages)-1].prevStdout → stageJoiners[len(stages)-1].nextStdin →
+//	   stage[len(stages)-1] →
+//	   stageJoiners[len(stages)].prevStdout == p.stdout
+//
+// In pseudo-Shell notation, the stages are run like this:
+//
+//	stage[0] <stageJoiners[0].nextStdin >stageJoiners[1].prevStdout
+//	stage[1] <stageJoiners[1].nextStdin >stageJoiners[2].prevStdout
+//	stage[2] <stageJoiners[2].nextStdin >stageJoiners[3].prevStdout
+//	   ...
+//	stage[i] <stageJoiners[i-1].nextStdin >stageJoiners[i].prevStdout
+//	   ...
+//	stage[len(stages)-1] <stageJoiners[len(stages)-1].nextStdin >p.stdout
+type stageJoiner struct {
+	// prevStage holds the stage that needs to write to the pipe.
+	prevStage Stage
+
+	// prevStdout will be used as the stdout of `prevStage`. It is
+	// usually the "write" end of the `(nextStdin, prevStdout)` pipe
+	// pair, with the connected pipe ends in the same `stageJoiner`
+	// instance.
+	prevStdout *OutputStream
+
+	// nextStage holds the stage that needs to read from the pipe.
+	nextStage Stage
+
+	// nextStdin will be used as the stdin of `nextStage`. It is
+	// usually the "read" end of the `(nextStdin, prevStdout)` pipe
+	// pair.
+	nextStdin *InputStream
+}
+
+// needFilePipe returns `true` if the pipe that joins the two adjacent
+// stages should be an `os.Pipe()` rather than an `io.Pipe()`.
+func (sj *stageJoiner) needFilePipe() bool {
+	if sj.prevStage.Requirements().Stdout == StreamPreferFile {
+		return true
+	}
+	if sj.nextStage.Requirements().Stdin == StreamPreferFile {
+		return true
+	}
+	return false
+}
+
+func (sj *stageJoiner) createPipe() error {
+	var r io.ReadCloser
+	var w io.WriteCloser
+	if sj.needFilePipe() {
+		var err error
+		r, w, err = os.Pipe()
+		if err != nil {
+			return fmt.Errorf("creating os.Pipe: %w", err)
+		}
+	} else {
+		r, w = io.Pipe()
+	}
+
+	sj.prevStdout = ClosingOutput(w)
+	sj.nextStdin = ClosingInput(r)
+
+	return nil
+}
+
+// closePipe closes both ends of the pipe that was allocated by
+// `createPipe()`. This should only be called if the corresponding
+// stage's `Start()` method was never called (otherwise the stage is
+// responsible for closing its stdin and stdout).
+func (sj *stageJoiner) closePipe() error {
+	return errors.Join(
+		sj.prevStdout.Close(),
+		sj.nextStdin.Close(),
+	)
+}
+
+// validate verifies that `sj.prevStdout` and `sj.nextStdin` are
+// suitable for the adjacent stages, in particular that no pipe is
+// created if the stage requirements are `StreamForbidden`.
+func (sj *stageJoiner) validate() error {
+	if sj.prevStage != nil {
+		stdoutRequirements := sj.prevStage.Requirements().Stdout
+		if stdoutRequirements == StreamForbidden && sj.prevStdout != nil {
+			return fmt.Errorf(
+				"stage %q forbids stdout, but stdout is connected", sj.prevStage.Name(),
+			)
+		}
+	}
+
+	if sj.nextStage != nil {
+		stdinRequirements := sj.nextStage.Requirements().Stdin
+		if stdinRequirements == StreamForbidden && sj.nextStdin != nil {
+			return fmt.Errorf(
+				"stage %q forbids stdin, but stdin is connected", sj.nextStage.Name(),
+			)
+		}
+	}
+
+	return nil
+}
